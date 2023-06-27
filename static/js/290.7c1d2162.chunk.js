58544be24557e98a0f8e00479eea012374e65c8f
ELENA88555 <113532568+ELENA88555@users.noreply.github.com>
2023-06-27 05:34:05 +0000

    Deploying to gh-pages from @ ELENA88555/goit-react-hw-08-phonebook@4ecc45780003707d3a35ecf2b3ce0e455e2d9055 🚀

--- static/js/290.7c1d2162.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[290],{7290:function(e,a,r){r.r(a),r.d(a,{default:function(){return c}});var t="Home_homePage__gNdkv",n="Home_title__L087v",s=r(184);function c(){return(0,s.jsx)("div",{className:t,children:(0,s.jsxs)("h1",{className:n,children:["Your PHONEBOOCK",(0,s.jsx)("span",{role:"img","aria-label":"Greeting icon"})]})})}}}]);
-//# sourceMappingURL=290.7c1d2162.chunk.js.map
